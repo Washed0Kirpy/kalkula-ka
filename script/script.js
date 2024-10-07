e5c1149aed03040436889bb68ed0d9eff09cff09
Jan Kirpal <Jan.Kirpal@student.souepl.cz>
2024-10-07 09:29:58 +0200

    kgu

---
+++ script/script.js
@@ -0,0 +1,15 @@
+let input = document.getElementById("input")
+
+//console.log("Prvek v inputu je:", input):
+
+function Add(){
+    input.value += Element;
+}
+function sum(){
+    vysledek = eval(input.value);
+    input.value += " = " + vysledek;
+}
+
+function del(){
+    input.value ="";
+}
